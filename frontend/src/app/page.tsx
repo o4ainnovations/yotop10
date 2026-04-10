ea642cfd91f497bc125871b7550eccf504a8f83e
nekwasar <lindawheek@gmail.com>
2026-04-10 06:51:08 -0400

    fix lint warning

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
