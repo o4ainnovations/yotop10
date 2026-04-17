1b7ca7d81951498471168f3fe4355fe974f6dc04
Nekwasar <lindawheek@gmail.com>
2026-04-17 22:42:06 +0000

    [Config] Disable ESLint during production builds

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
