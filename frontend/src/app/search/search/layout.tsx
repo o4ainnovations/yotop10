9b566839492032e4fd102572268c457a49d8bc9c
Nekwasar <lindawheek@gmail.com>
2026-05-09 12:02:23 +0200

    [Fix] Remove recreated nested search/search directory

--- frontend/src/app/search/search/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from 'react';
-
-export default function SearchLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <Suspense fallback={<div style={{ padding: '40px', textAlign: 'center', color: '#999' }}>Loading search...</div>}>
-      {children}
-    </Suspense>
-  );
-}
